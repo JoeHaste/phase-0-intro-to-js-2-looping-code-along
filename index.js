9ad7e5ba237c695a62064d5cdbc0c338eb542c0e
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-06-18 21:46:48 +0300

    complete assignement

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+writeCards(names);
+
+function countDown(number) {
+    let n = number;
+    while (n >= 0) {
+      console.log(n--);
+    }
+}
+
+countDown(10)
